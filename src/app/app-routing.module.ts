6a869dd9d325c61f04cad774ccab55536007d536
philipibovo <philipi@bovo.me>
2020-06-15 17:49:43 -0300

    Remove template page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-
 //Pages > Home
 import { HomeComponent } from './pages/home/home.component';
 //Pages > Studio
